6a3912f39f86fc60ec46c154e96e06e7878d7d07
Koorosh <mr.oplus@yahoo.fr>
2020-10-05 05:44:53 +0330

    Migrated from Arduino Nano to NodeMCU ESP8266
    YES ! I KNOW ! it's INSECURE but i use this little hardware behind 2 firewalls and n to n NAT so don't blame me :)

--- RoomControl.ino
+++ RoomControl.ino
@@ -1,118 +1,149 @@
-#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
 
-SoftwareSerial BTSerial(10, 11);   // RX | TX
-int relays[] = {2,3};
-const int BUTTON_1 = 8;
-const int BUTTON_2 = 9;
+const char* ssid     = "Oplus - Micro Devices";
+const char* password = "mr.oplus@yahoo.fr";
+WiFiServer server(80);
+String header;
 
-int command[4];
-int pos = 0  ; 
-bool shouldProcess = false;
-volatile bool RELAY1_STATE = true;
-volatile bool RELAY2_STATE = true;
-volatile bool FROM_BUTTON1 = false;
-volatile bool FROM_BUTTON2 = false;
-void sendCommand(const char * command){
-  Serial.println(command);
-  BTSerial.println(command);
-  delay(100);
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("Starting ..."); 
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D0, INPUT);
+  pinMode(D1, INPUT);
+  digitalWrite(D2 ,HIGH); 
+  digitalWrite(D3 ,HIGH);
+  Serial.println("Turning OFF all relays"); 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
 }
-
-void setup() 
-{
-  BTSerial.begin(9600);
-  sendCommand("AT+NAMEOplusRoom");
-  sendCommand("AT+ROLE0");
-  sendCommand("AT+UUIDFFE0");
-  sendCommand("AT+CHARFFE1");
-  pinMode(relays[0],OUTPUT);
-  pinMode(relays[1],OUTPUT);
-  pinMode(BUTTON_1,INPUT);
-  pinMode(BUTTON_2,INPUT);
-  digitalWrite(relays[0],HIGH);
-  digitalWrite(relays[1],HIGH);
+bool STATE1 = false;
+bool STATE2 = false;
+void loop() {
+  int mode1 = digitalRead(D0); 
+  int mode2 = digitalRead(D1); 
+  if(mode1 == HIGH){
+    STATE1= !STATE1;
+    swapState(D2,STATE1);
+  }
+  if(mode2 == HIGH){
+    STATE2= !STATE2;
+    swapState(D3,STATE2);
+  }
+  HandleWifi();
+  delay(1000);
 }
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
+const long timeoutTime = 2000;
+void HandleWifi(){
+  WiFiClient client = server.available();
+  if (client) {                            
+    Serial.println("New Client.");          
+    String currentLine = "";
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {
+      currentTime = millis();         
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);
+        header += c;
+        if (c == '\n') { 
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            if (header.indexOf("GET /1/on") >= 0) {
+              Serial.println("REALY 1 on");
+              STATE1 = true;
+              digitalWrite(D2, LOW);
+            } else if (header.indexOf("GET /1/off") >= 0) {
+              Serial.println("RELAY 1 off");
+              STATE1 = false;
+              digitalWrite(D2, HIGH);
+            } else if (header.indexOf("GET /2/on") >= 0) {
+              Serial.println("RELAY 2 on");
+              STATE2 = true;;
+              digitalWrite(D3, LOW);
+            } else if (header.indexOf("GET /2/off") >= 0) {
+              Serial.println("RELAY 2 off");
+              STATE2 = false;
+              digitalWrite(D3, HIGH);
+            }
+            
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            client.println("<body><h1>Oplus Room</h1>");
+            if (STATE1){
+              client.println("<p>RELAY1 - State ON</p>");  
+            }else{
+              client.println("<p>RELAY1 - State OFF</p>");  
+            }
+            
+            if (STATE1 == false) {
+              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            if (STATE2){
+              client.println("<p>RELAY2 - State ON</p>");  
+            }else{
+              client.println("<p>RELAY2 - State OFF</p>");  
+            }
 
-void logCommand(const char * str){
-  Serial.println(str);
-}
-void readBLECommand(){
-  while(BTSerial.available() && !shouldProcess){
-    char dt = BTSerial.read(); 
-    int b=  (int) dt;
-    if(b == 00 && pos == 0){
-        command[pos++] = b;
-    }else{
-      if(b == -1 && pos == 3){
-        command[pos++] = b;
-        shouldProcess = true;
-        pos = 0 ; 
-      }else if ( pos == 1 || pos == 2 ){
-        command[pos++] = b;
+            if (STATE2==false) {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
       }
     }
-  }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }  
 }
-void processCommand(){
-  Serial.print("Relay : ");
-  Serial.print(command[1]);
-  Serial.print(" Mode : " ); 
-  Serial.println(command[2]);
-  
-  int relayNumber = command[1];
-  int relayMode = command[2]; 
-
-  if (relayNumber == 0 || relayNumber == 1){
-    if(relayMode == 0|| relayMode == 1 ){
-      if (relayMode == 1){
-         digitalWrite(relays[relayNumber],HIGH);
-      }else if (relayMode == 0){
-        digitalWrite(relays[relayNumber],LOW);
-      }
-      digitalWrite(13,HIGH);
-      delay(500);
-      digitalWrite(13,LOW);
-      delay(500);
-      digitalWrite(13,HIGH);
-    }
-  }
-  shouldProcess = false;
-  pos = 0 ;
-}
-void loop()
-{
-  int r1 = digitalRead(BUTTON_1);
-  int r2 = digitalRead(BUTTON_2);
-  if(r1 == HIGH){
-    RELAY1_STATE = !RELAY1_STATE;
-    FROM_BUTTON1 = true;
-  }
-  if(r2 == HIGH){
-    RELAY2_STATE = !RELAY2_STATE;
-    FROM_BUTTON2 = true;
-  }
-  if(FROM_BUTTON1){
-    FROM_BUTTON1 = false;
-    Serial.println("SWAPPING RELAY 1");
-    if(RELAY1_STATE){
-      digitalWrite(relays[0],HIGH);
-    }else{
-      digitalWrite(relays[0],LOW);
-    }
-    delay(1000);
-  }
-  if(FROM_BUTTON2){
-    FROM_BUTTON2 = false;
-    Serial.println("SWAPPING RELAY 2");
-    if(RELAY2_STATE){
-      digitalWrite(relays[1],HIGH);
-    }else{
-      digitalWrite(relays[1],LOW);
-    }
-    delay(1000);
+void swapState(int pinNumber , bool mod){
+  Serial.print("Swapping PinNumber#");
+  Serial.print(pinNumber);
+  Serial.print(" New Mode : " );
+  Serial.print(mod);
+  Serial.println();
+  if(mod == true){
+    digitalWrite(pinNumber,LOW);
+  }else{
+    digitalWrite(pinNumber,HIGH);
   }
   
-  readBLECommand(); 
-  if(shouldProcess)
-    processCommand();
 }
